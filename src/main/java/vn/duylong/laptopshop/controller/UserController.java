79f9262dadd3af1d44b8cb9b6af8879a3a00aacd
Duy Long <longngo2407@gmail.com>
2025-04-11 09:59:37 +0700

    Chapter 7

---
+++ src/main/java/vn/duylong/laptopshop/controller/UserController.java
@@ -0,0 +1,40 @@
+package vn.duylong.laptopshop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import vn.duylong.laptopshop.domain.User;
+import vn.duylong.laptopshop.service.UserService;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/")
+    public String getHomePage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("ong", test);
+        model.addAttribute("longngo", "from controller with model");
+        return "hello";
+    }
+
+    @RequestMapping("/admin/user") // GET
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User duylong) {
+        System.out.println("Run here" + duylong);
+        return "hello";
+    }
+}
